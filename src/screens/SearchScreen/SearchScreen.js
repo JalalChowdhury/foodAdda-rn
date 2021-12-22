51b696afec3ea2d6a9eb1e44ae893dc0734c9f26
Md. Jalal Uddin Chowdhury <JalalChowdhuryCSE1999@gmail.com>
2021-12-23 00:11:13 +0600

    first commit

---
+++ src/screens/SearchScreen/SearchScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function SearchScreen() {
+    return (
+        <View>
+            <Text>Apna time ayega</Text>
+        </View>
+    );
+}
+
+export default SearchScreen;
