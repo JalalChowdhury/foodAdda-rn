d429e8b82fa58f3a1c636ba7bbff4e25eea7505b
Md. Jalal Uddin Chowdhury <JalalChowdhuryCSE1999@gmail.com>
2021-12-23 01:19:41 +0600

    added navigator

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator ,createAppContainer } from 'react-navigation';
+import SearchScreen from './src/screens/SearchScreen/SearchScreen';
 
 const navigator = createStackNavigator({
 
+  Search : SearchScreen
+
+}, {
+  intialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Food Adda'
+  }
 });
+
+export default createAppContainer(navigator);
